c1304653041a8113326d6c518c31820420f4d676
Tyler <tyler_venetsanos@msn.com>
2021-04-13 10:28:12 -0400

    commit tree f95cf1ba21909bd108cd90c3c4d35794591b012a

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Your code goes here
+function loadDom(){
+  text = document.getElementById("text")
+  text.innerHTML = "This is really cool!";
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    loadDom()
+  });
